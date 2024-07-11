cb5c51a692486323e347df930960cf65d27f6220
Cris Mouta <82060703+crismouta@users.noreply.github.com>
2024-07-11 15:18:00 +0200

    abstract clases and interfaces

--- src/App.java
+++ src/App.java
@@ -4,6 +4,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         Emotion joy = new Emotion("Joy", 13, "yellow", true, 10);
         System.out.println(joy.introduce());
-        System.out.println(joy.eat());
     }
 }
